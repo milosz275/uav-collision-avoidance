825491a106fed2fcb20c802b797bc4be6e31317d
Miłosz Maculewicz <miloszmaculewicz@gmail.com>
2024-02-09 08:47:36 +0100

    Initial commit

--- python_package_template/__init__.py
+++
@@ -1,3 +0,0 @@
-# __init__.py
-from .main import main
-from src.classname import Classname
--- python_package_template/src/classname.py
+++
@@ -1,7 +0,0 @@
-# classname.py
-
-class Classname():
-    """Example class"""
-
-    def __init__(self) -> None:
-        pass
