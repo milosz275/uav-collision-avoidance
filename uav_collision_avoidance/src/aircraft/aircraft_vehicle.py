9d90f11fb9f53a747718eb204241eb268b287198
Miłosz Maculewicz <miloszmaculewicz@gmail.com>
2024-02-16 18:10:07 +0100

    Add position and speed as QVector3D

--- uav_collision_avoidance/src/aircraft/aircraft_vehicle.py
+++ uav_collision_avoidance/src/aircraft/aircraft_vehicle.py
@@ -3,16 +3,20 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton
 from PySide6.QtCore import Qt, QObject, QThread, Signal
-from PySide6.QtGui import QPainter, QColor, QBrush, QKeyEvent
+from PySide6.QtGui import QPainter, QColor, QBrush, QKeyEvent, QVector3D
+
+from src.simulation.simulation_state import SimulationState
 
 class AircraftVehicle(QObject):
+    """Aircraft physical UAV"""
+
     positionChanged = Signal(float, float)
 
-    def __init__(self, x : float, y : float, speed : float) -> None:
+    def __init__(self, position : QVector3D, speed : QVector3D, state : SimulationState) -> None:
         super().__init__()
-        self.x = x
-        self.y = y
+        self.position = position
         self.speed = speed
+        self.state = state
         return
 
     def move(self, dx, dy) -> None:
