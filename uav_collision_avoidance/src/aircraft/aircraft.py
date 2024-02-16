7e9e9e5c7b4c12cec53dd296ea821fb2e04f13b0
Miłosz Maculewicz <miloszmaculewicz@gmail.com>
2024-02-16 18:09:39 +0100

    Add state, add comment

--- uav_collision_avoidance/src/aircraft/aircraft.py
+++ uav_collision_avoidance/src/aircraft/aircraft.py
@@ -2,11 +2,12 @@
 
 from src.aircraft.aircraft_render import AircraftRender
 from src.aircraft.aircraft_vehicle import AircraftVehicle
+from src.simulation.simulation_state import SimulationState
 
 class Aircraft():
-    """Aircraft"""
+    """Main aircraft class"""
     
-    def __init__(self, x : float, y : float, color : str) -> None:
-        self.vehicle = AircraftVehicle(x=x, y=y, speed=100)
-        self.render = AircraftRender(color=color, vehicle=self.vehicle)
+    def __init__(self, x : float, y : float, color : str, state : SimulationState) -> None:
+        self.vehicle = AircraftVehicle(x=x, y=y, speed=100, state=state)
+        self.render = AircraftRender(color=color, vehicle=self.vehicle, state=state)
         return
