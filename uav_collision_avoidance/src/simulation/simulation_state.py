2c3694ce8cc5e910e48f40c74f79cfdeb7a1c6da
Miłosz Maculewicz <miloszmaculewicz@gmail.com>
2024-02-16 18:13:23 +0100

    Add different state settings, include assets

--- uav_collision_avoidance/src/simulation/simulation_state.py
+++ uav_collision_avoidance/src/simulation/simulation_state.py
@@ -1,13 +1,23 @@
 # simulation_state.py
 
+from PySide6.QtGui import QPixmap
+
 class SimulationState:
-    def __init__(self) -> None:
-        self._is_paused : bool = False
+    """Class defining simulation's traits"""
+
+    def __init__(self, simulation_threshold : int) -> None:
+        # simulation state
+        self.simulation_threshold = simulation_threshold
+        self.is_paused : bool = False
+
+        # render state
+        self.scale : float = 1.0
+
+        # assets
+        self.aircraft_image = QPixmap()
+        self.aircraft_image.load("src/assets/aircraft.png")
         return
-    
-    def is_paused(self) -> bool:
-        return self._is_paused
 
     def toggle_pause(self) -> None:
-        self._is_paused = not self._is_paused
+        self.is_paused = not self.is_paused
         return
