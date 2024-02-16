31e37548b2d7f4d7e53e0d7adcd81a57fd477a02
Miłosz Maculewicz <miloszmaculewicz@gmail.com>
2024-02-16 19:45:41 +0100

    Add third dimension

--- uav_collision_avoidance/src/aircraft/aircraft.py
+++ uav_collision_avoidance/src/aircraft/aircraft.py
@@ -1,5 +1,7 @@
 # aircraft.py
 
+from PySide6.QtGui import QVector3D
+
 from src.aircraft.aircraft_render import AircraftRender
 from src.aircraft.aircraft_vehicle import AircraftVehicle
 from src.simulation.simulation_state import SimulationState
@@ -7,7 +9,7 @@ from src.simulation.simulation_state import SimulationState
 class Aircraft():
     """Main aircraft class"""
     
-    def __init__(self, x : float, y : float, color : str, state : SimulationState) -> None:
-        self.vehicle = AircraftVehicle(x=x, y=y, speed=100, state=state)
+    def __init__(self, x : float, y : float, height : float, color : str, state : SimulationState) -> None:
+        self.vehicle = AircraftVehicle(position=QVector3D(x, y, height), speed=QVector3D(50, 0 , 50), state=state)
         self.render = AircraftRender(color=color, vehicle=self.vehicle, state=state)
         return
