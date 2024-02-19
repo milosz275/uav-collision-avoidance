483afbffb5a41a1d64c3bc451a1fd1ccf9da90bb
Miłosz Maculewicz <miloszmaculewicz@gmail.com>
2024-02-19 19:49:06 +0100

    Add methods comments, remove unused imports

--- uav_collision_avoidance/src/simulation/simulation_physics.py
+++ uav_collision_avoidance/src/simulation/simulation_physics.py
@@ -1,10 +1,7 @@
 # simulation_physics.py
 
-import sys
 from typing import List
-from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton
-from PySide6.QtCore import Qt, QObject, QThread, Signal
-from PySide6.QtGui import QPainter, QColor, QBrush, QKeyEvent
+from PySide6.QtCore import QThread, QTime
 
 from src.aircraft.aircraft_vehicle import AircraftVehicle
 from src.simulation.simulation_state import SimulationState
@@ -16,12 +13,17 @@ class SimulationPhysics(QThread):
         super(SimulationPhysics, self).__init__(parent)
         self.aircrafts = aircrafts
         self.simulation_state = simulation_state
+        self.previous_timestamp = QTime.currentTime()
         return
         
     def run(self) -> None:
+        """Runs physics simulation thread"""
         while not self.isInterruptionRequested():
+            start_timestamp = QTime.currentTime()
             if not self.simulation_state.is_paused:
                 for aircraft in self.aircrafts:
+                    # todo: calculate move deltas using previous and start timestamps
                     aircraft.move(1, 1, 0)
-            self.msleep(self.simulation_state.simulation_threshold)
+                self.previous_timestamp = QTime.currentTime()
+            self.msleep(max(0, (self.simulation_state.simulation_threshold) - start_timestamp.msecsTo(QTime.currentTime())))
         return super().run()
