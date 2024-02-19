e173bfa637aab138926456a2121231c4527e3752
Miłosz Maculewicz <miloszmaculewicz@gmail.com>
2024-02-19 20:08:51 +0100

    Fix args usage

--- uav_collision_avoidance/main.py
+++ uav_collision_avoidance/main.py
@@ -5,12 +5,12 @@ from PySide6.QtWidgets import QApplication
 from src.simulation.simulation_settings import SimulationSettings
 from src.simulation.simulation import Simulation
 
-def main():
+def main(args):
     """Executes main function"""
-    app = QApplication(sys.argv)
+    app = QApplication(args)
     SimulationSettings.screen_resolution = app.primaryScreen().size()
     sim = Simulation()
     sys.exit(app.exec())
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
