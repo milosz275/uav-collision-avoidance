af885f37c3360d325779e929114fcf8e9848b261
Miłosz Maculewicz <miloszmaculewicz@gmail.com>
2024-02-19 14:15:44 +0100

    Add horizontal and vertical speed calculation

--- uav_collision_avoidance/src/aircraft/aircraft_vehicle.py
+++ uav_collision_avoidance/src/aircraft/aircraft_vehicle.py
@@ -13,8 +13,9 @@ class AircraftVehicle(QObject):
 
     positionChanged = Signal(float, float, float)
 
-    def __init__(self, position : QVector3D, speed : QVector3D, state : SimulationState) -> None:
+    def __init__(self, aircraft_id : int, position : QVector3D, speed : QVector3D, state : SimulationState) -> None:
         super().__init__()
+        self.aircraft_id = aircraft_id
         self.position = position
         if self.position.z() < 0:
             self.position.setZ(0)
@@ -35,6 +36,12 @@ class AircraftVehicle(QObject):
     
     def absolute_speed(self) -> float:
         return self.speed.length()
+    
+    def horizontal_speed(self) -> float:
+        return QVector3D(self.speed.x(), self.speed.y(), 0).length()
+    
+    def vertical_speed(self) -> float:
+        return QVector3D(0, 0, self.speed.z()).length()
 
     def yaw_angle(self) -> float:
         return degrees(atan2(self.speed.y(), self.speed.x()))
