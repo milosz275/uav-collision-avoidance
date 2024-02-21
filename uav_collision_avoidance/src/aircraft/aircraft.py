41f28105b6cfe7295467da641da70e7639dcd77a
Miłosz Maculewicz <miloszmaculewicz@gmail.com>
2024-02-21 11:49:25 +0100

    Move getting id to separate method

--- uav_collision_avoidance/src/aircraft/aircraft.py
+++ uav_collision_avoidance/src/aircraft/aircraft.py
@@ -13,8 +13,13 @@ class Aircraft(QObject):
     current_id : int = 0
 
     def __init__(self, x : float, y : float, height : float, color : str, state : SimulationState) -> None:
-        self.aircraft_id = Aircraft.current_id
-        Aircraft.current_id += 1
+        self.aircraft_id = self.get_id()
         self.vehicle = AircraftVehicle(self.aircraft_id, position=QVector3D(x, y, height), speed=QVector3D(50, 50 , 0), state=state)
         self.render = AircraftRender(color=color, vehicle=self.vehicle, state=state)
         return
+
+    def get_id(self):
+        """Gets unique id for the aircraft"""
+        aircraft_id = Aircraft.current_id
+        Aircraft.current_id += 1
+        return aircraft_id
