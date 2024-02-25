98685f632c600f8db4c703b34d43a93c93ce0a9d
Miłosz Maculewicz <miloszmaculewicz@gmail.com>
2024-02-25 17:58:49 +0100

    Apply movement basing on the speed

--- uav_collision_avoidance/src/simulation/simulation_physics.py
+++ uav_collision_avoidance/src/simulation/simulation_physics.py
@@ -1,8 +1,10 @@
 # simulation_physics.py
 
+import sys
 from typing import List
 
 from PySide6.QtCore import QThread, QTime
+from PySide6.QtGui import QVector3D
 
 from src.aircraft.aircraft_vehicle import AircraftVehicle
 from src.simulation.simulation_state import SimulationState
@@ -24,7 +26,17 @@ class SimulationPhysics(QThread):
             if not self.simulation_state.is_paused:
                 for aircraft in self.aircrafts:
                     # todo: calculate move deltas using previous and start timestamps
-                    aircraft.move(1, 1, 0)
+                    elapsed_time : float = self.previous_timestamp.msecsTo(start_timestamp)
+                    delta_pos : QVector3D = QVector3D(
+                        aircraft.speed.x() * elapsed_time,
+                        aircraft.speed.y() * elapsed_time,
+                        aircraft.speed.z() * elapsed_time,
+                    )
+                    aircraft.move(
+                        delta_pos.x() / 1000.0,
+                        delta_pos.y() / 1000.0,
+                        delta_pos.z() / 1000.0,
+                    )
                 self.previous_timestamp = QTime.currentTime()
             self.msleep(max(0, (self.simulation_state.simulation_threshold) - start_timestamp.msecsTo(QTime.currentTime())))
         return super().run()
