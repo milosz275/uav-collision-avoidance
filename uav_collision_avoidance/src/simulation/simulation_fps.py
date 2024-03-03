1a19c7ecdc32d428edfe43e1ab1e5559cf077eb4
Miłosz Maculewicz <miloszmaculewicz@gmail.com>
2024-03-03 19:38:44 +0100

    Remove signals, change counting method

--- uav_collision_avoidance/src/simulation/simulation_fps.py
+++ uav_collision_avoidance/src/simulation/simulation_fps.py
@@ -4,18 +4,15 @@ from PySide6.QtCore import QThread, QTime
 from PySide6.QtGui import QCloseEvent
 
 from src.simulation.simulation_state import SimulationState
-from src.simulation.simulation_render import SimulationRender
 
 class SimulationFPS(QThread):
     """Thread running frames per second counter"""
 
-    def __init__(self, parent, simulation_render : SimulationRender, simulation_state : SimulationState) -> None:
+    def __init__(self, parent, simulation_state : SimulationState) -> None:
         super(SimulationFPS, self).__init__(parent)
         self.simulation_state = simulation_state
-        self.simulation_render = simulation_render
         self.counted_frames : int = 0
         self.previous_timestamp = QTime.currentTime()
-        self.simulation_render.rendered.connect(self.count_frame)
         return
         
     def run(self) -> None:
@@ -24,7 +21,7 @@ class SimulationFPS(QThread):
             start_timestamp = QTime.currentTime()
             elapsed_time : float = self.previous_timestamp.msecsTo(start_timestamp)
             if self.counted_frames > 0:
-                self.simulation_state.fps = self.counted_frames / elapsed_time * 500
+                self.simulation_state.fps = self.counted_frames / elapsed_time * 1000
                 self.counted_frames = 0
             else:
                 self.simulation_state.fps = 0.0
@@ -38,6 +35,5 @@ class SimulationFPS(QThread):
 
     def closeEvent(self, event: QCloseEvent) -> None:
         """Qt method performed on the main window close event"""
-        self.simulation_render.rendered.disconnect(self.count_frame)
         event.accept()
         return super().closeEvent(event)
