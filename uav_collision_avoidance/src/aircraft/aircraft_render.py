54784e6f714d3624c6f5a5887e41e2ceb5f31a88
Miłosz Maculewicz <miloszmaculewicz@gmail.com>
2024-03-03 19:36:42 +0100

    Remove signals

--- uav_collision_avoidance/src/aircraft/aircraft_render.py
+++ uav_collision_avoidance/src/aircraft/aircraft_render.py
@@ -1,20 +1,19 @@
 # aircraft_render.py
 
-from PySide6.QtCore import QObject, Signal
+from PySide6.QtCore import QObject
 from PySide6.QtGui import QVector3D
 
-from src.aircraft.aircraft_vehicle import AircraftVehicle
 from src.aircraft.aircraft_fcc import AircraftFCC
+from src.aircraft.aircraft_vehicle import AircraftVehicle
 from src.simulation.simulation_state import SimulationState
 
 class AircraftRender(QObject):
     """Aircraft graphical representation"""
 
-    position_changed = Signal(float, float, float)
-
     def __init__(self, aircraft_id : int, vehicle : AircraftVehicle, fcc : AircraftFCC, state : SimulationState) -> None:
         super().__init__()
         self.aircraft_id = aircraft_id
+        self.vehicle = vehicle
         self.state = state
         self.fcc = fcc
 
@@ -25,10 +24,6 @@ class AircraftRender(QObject):
         self.pitch_angle : float = 0.0
         self.roll_angle : float = 0.0
         self.safezone_occupied : bool = False
-
-        if vehicle:
-            self.vehicle = vehicle
-            self.vehicle.position_changed.connect(self.update)
         return
     
     def move(self, dx : float, dy : float, dz : float = 0.0) -> None:
@@ -36,26 +31,12 @@ class AircraftRender(QObject):
         self.vehicle.move(dx, dy, dz)
         return
 
-    def set_vehicle(self, vehicle) -> None:
-        """Sets the physical aircraft for rendered aircraft"""
-        if not self.vehicle:
-            self.vehicle = vehicle
-            self.vehicle.position_changed.connect(self.update)
-        return
-    
-    def disconnect_vehicle(self) -> None:
-        """Disconnects active vehicle from renderer"""
-        if self.vehicle:
-            self.vehicle.position_changed.disconnect(self.update)
-        return
-
     def update(self) -> None:
         """Updates graphical location of the render based on vehicle position"""
-        self.position.setX(self.vehicle.position.x() * self.state.scale)
-        self.position.setY(self.vehicle.position.y() * self.state.scale)
-        self.position.setZ(self.vehicle.position.z() * self.state.scale)
-        self.position_changed.emit(self.position.x(), self.position.y(), self.position.z())
-        self.size = self.vehicle.size * self.state.scale
+        self.position.setX(self.vehicle.position.x() * self.state.gui_scale)
+        self.position.setY(self.vehicle.position.y() * self.state.gui_scale)
+        self.position.setZ(self.vehicle.position.z() * self.state.gui_scale)
+        self.size = self.vehicle.size * self.state.gui_scale
         self.yaw_angle = self.vehicle.yaw_angle()
         self.pitch_angle = self.vehicle.pitch_angle()
         self.roll_angle = self.vehicle.roll_angle
