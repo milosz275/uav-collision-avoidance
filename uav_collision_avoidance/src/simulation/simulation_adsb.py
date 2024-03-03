ef18515f0a4ca57e639a4258c112885c2eaac8db
Miłosz Maculewicz <miloszmaculewicz@gmail.com>
2024-03-03 19:38:12 +0100

    Set one UAV tracking

--- uav_collision_avoidance/src/simulation/simulation_adsb.py
+++ uav_collision_avoidance/src/simulation/simulation_adsb.py
@@ -14,6 +14,7 @@ class SimulationADSB(QThread):
         super(SimulationADSB, self).__init__(parent)
         self.aircrafts = aircrafts
         self.simulation_state = simulation_state
+        self.adsb_cycles : int = 0
         return
         
     def run(self) -> None:
@@ -21,13 +22,19 @@ class SimulationADSB(QThread):
         while not self.isInterruptionRequested():
             start_timestamp = QTime.currentTime()
             if not self.simulation_state.is_paused:
-                for aircraft in self.aircrafts:
-                    print("Aircraft id: " + str(aircraft.aircraft_id) +
-                          "; speed: " + "{:.2f}".format(aircraft.absolute_speed()) +
-                          "; yaw angle: " + "{:.2f}".format(aircraft.yaw_angle()) +
-                          "; pitch angle: " + "{:.2f}".format(aircraft.pitch_angle()) +
-                          "; roll angle: " + "{:.2f}".format(aircraft.roll_angle) +
-                          "; distance covered: " + "{:.2f}".format(aircraft.distance_covered) +
-                          "; fps: " + "{:.2f}".format(self.simulation_state.fps))
+                self.adsb_cycles += 1
+                aircrafts = self.aircrafts.copy()
+                for aircraft in aircrafts:
+                    if aircraft.aircraft_id == 0:
+                        print("Aircraft id: " + str(aircraft.aircraft_id) +
+                            "; speed: " + "{:.2f}".format(aircraft.absolute_speed()) +
+                            "; x: " + "{:.2f}".format(aircraft.position.x()) +
+                            "; yaw angle: " + "{:.2f}".format(aircraft.yaw_angle()) +
+                            "; pitch angle: " + "{:.2f}".format(aircraft.pitch_angle()) +
+                            "; roll angle: " + "{:.2f}".format(aircraft.roll_angle) +
+                            "; distance covered: " + "{:.2f}".format(aircraft.distance_covered) +
+                            "; fps: " + "{:.2f}".format(self.simulation_state.fps) +
+                            "; t: " + str(self.adsb_cycles) +
+                            "; phys: " + str(self.simulation_state.physics_cycles))
             self.msleep(max(0, self.simulation_state.adsb_threshold - start_timestamp.msecsTo(QTime.currentTime())))
         return super().run()
