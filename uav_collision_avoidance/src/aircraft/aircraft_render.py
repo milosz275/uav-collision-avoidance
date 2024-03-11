8ad9f02975c9fb897a8e64cde98b75c6ea3b7360
mldxo <miloszmaculewicz@gmail.com>
2024-03-11 11:06:11 +0100

    Add methods usage

--- uav_collision_avoidance/src/aircraft/aircraft_render.py
+++ uav_collision_avoidance/src/aircraft/aircraft_render.py
@@ -33,12 +33,12 @@ class AircraftRender(QObject):
 
     def update(self) -> None:
         """Updates graphical location of the render based on vehicle position"""
-        self.position.setX(self.vehicle.position.x() * self.state.gui_scale)
-        self.position.setY(self.vehicle.position.y() * self.state.gui_scale)
-        self.position.setZ(self.vehicle.position.z() * self.state.gui_scale)
-        self.size = self.vehicle.size * self.state.gui_scale
+        self.position.setX(self.vehicle.position().x() * self.state.gui_scale)
+        self.position.setY(self.vehicle.position().y() * self.state.gui_scale)
+        self.position.setZ(self.vehicle.position().z() * self.state.gui_scale)
+        self.size = self.vehicle.size() * self.state.gui_scale
         self.yaw_angle = self.vehicle.yaw_angle()
         self.pitch_angle = self.vehicle.pitch_angle()
-        self.roll_angle = self.vehicle.roll_angle
+        self.roll_angle = self.vehicle.roll_angle()
         self.safezone_occupied = self.fcc.safezone_occupied
         return
