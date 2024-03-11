3e7bae69d9f99cc65f50001bc5a1289ca4ab145d
mldxo <miloszmaculewicz@gmail.com>
2024-03-11 11:06:57 +0100

    Change obtain id name, add private members

--- uav_collision_avoidance/src/aircraft/aircraft.py
+++ uav_collision_avoidance/src/aircraft/aircraft.py
@@ -14,13 +14,19 @@ class Aircraft(QObject):
     current_id : int = 0
 
     def __init__(self, x : float, y : float, height : float, state : SimulationState) -> None:
-        self.aircraft_id = self.get_id()
-        self.fcc = AircraftFCC()
-        self.vehicle = AircraftVehicle(self.aircraft_id, position=QVector3D(x, y, height), speed=QVector3D(100, 0, 0), fcc=self.fcc, state=state)
-        self.render = AircraftRender(self.aircraft_id, vehicle=self.vehicle, fcc=self.fcc, state=state)
+        self.__aircraft_id = self.obtain_id()
+        self.__fcc = AircraftFCC()
+        self.__vehicle = AircraftVehicle(self.__aircraft_id, position=QVector3D(x, y, height), speed=QVector3D(100, 0, 0), fcc=self.__fcc, state=state)
+        self.__render = AircraftRender(self.__aircraft_id, vehicle=self.__vehicle, fcc=self.__fcc, state=state)
         return
+    
+    def vehicle(self):
+        return self.__vehicle
+    
+    def render(self):
+        return self.__render
 
-    def get_id(self):
+    def obtain_id(self):
         """Gets unique id for the aircraft"""
         aircraft_id = Aircraft.current_id
         Aircraft.current_id += 1
