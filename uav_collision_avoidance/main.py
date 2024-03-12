7c583252a1292738719fd9607ab7a4a4cc1af640
mldxo <miloszmaculewicz@gmail.com>
2024-03-12 23:45:08 +0100

    Add logs creation, config logging, add platform and app start logs

--- uav_collision_avoidance/main.py
+++ uav_collision_avoidance/main.py
@@ -3,13 +3,21 @@
 with open("../version", "r") as file:
     version = file.read()
 
+from pathlib import Path
+Path("logs").mkdir(parents=True, exist_ok=True)
+
+import logging
+logging.basicConfig(filename="logs/simulator.log", level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(filename)s - %(message)s")
+
 import sys
 import platform
 if platform.system() == 'Windows':
     import ctypes
     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(f'io.github.mldxo.uav-collision-avoidance.{version}')
+logging.info(f"Detected platform: {platform.system()}")
 
 from PySide6.QtWidgets import QApplication
+
 from src.simulation.simulation_settings import SimulationSettings
 from src.simulation.simulation import Simulation
 
@@ -19,6 +27,7 @@ def main(args):
     app.setApplicationName("UAV Collsion Avoidance")
     app.setApplicationVersion(version)
     SimulationSettings.screen_resolution = app.primaryScreen().size()
+    logging.info("Creating simulation")
     sim = Simulation()
     sim.run_realtime()
     sys.exit(app.exec())
