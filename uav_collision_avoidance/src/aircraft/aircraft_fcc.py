3d20d1e289433ae685a77974c70f1ee199d4da98
mldxo <miloszmaculewicz@gmail.com>
2024-03-12 23:45:29 +0100

    Set default roll angle

--- uav_collision_avoidance/src/aircraft/aircraft_fcc.py
+++ uav_collision_avoidance/src/aircraft/aircraft_fcc.py
@@ -14,8 +14,9 @@ class AircraftFCC(QObject):
         self.safezone_occupied : bool = False
 
         self.target_yaw_angle : float = 0.0
-        self.target_roll_angle : float = 0.0
+        self.target_roll_angle : float = 30.0
         self.target_pitch_angle : float = 0.0
+        # self.target_speed : float = 100.0
 
         self.points : List[QVector3D] = [
             QVector3D(10000, 10000, 1000),
