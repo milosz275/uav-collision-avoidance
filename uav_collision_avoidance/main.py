5f71869982c3a3367e2b44b1c9a3243ad93cd9cf
mldxo <miloszmaculewicz@gmail.com>
2024-03-12 10:13:23 +0100

    Read version from file

--- uav_collision_avoidance/main.py
+++ uav_collision_avoidance/main.py
@@ -1,10 +1,13 @@
 # main.py
 
+with open("../version", "r") as file:
+    version = file.read()
+
 import sys
 import platform
 if platform.system() == 'Windows':
     import ctypes
-    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('io.github.mldxo.uav-collision-avoidance.1.0')
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(f'io.github.mldxo.uav-collision-avoidance.{version}')
 
 from PySide6.QtWidgets import QApplication
 from src.simulation.simulation_settings import SimulationSettings
@@ -14,7 +17,7 @@ def main(args):
     """Executes main function"""
     app = QApplication(args)
     app.setApplicationName("UAV Collsion Avoidance")
-    app.setApplicationVersion("1.0")
+    app.setApplicationVersion(version)
     SimulationSettings.screen_resolution = app.primaryScreen().size()
     sim = Simulation()
     sim.run_realtime()
