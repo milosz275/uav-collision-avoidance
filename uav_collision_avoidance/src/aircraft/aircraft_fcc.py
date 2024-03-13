a03b136dceca8ef37f308cc266360ab8419a13a7
mldxo <miloszmaculewicz@gmail.com>
2024-03-13 23:00:18 +0100

    Get id, set defaults, add destinations history, add target yaw and roll angle calculations

--- uav_collision_avoidance/src/aircraft/aircraft_fcc.py
+++ uav_collision_avoidance/src/aircraft/aircraft_fcc.py
@@ -1,5 +1,6 @@
 # aircraft_fcc.py
 
+import logging
 from copy import copy
 from typing import List
 from math import dist, atan2, degrees
@@ -12,9 +13,9 @@ from src.aircraft.aircraft_vehicle import AircraftVehicle
 class AircraftFCC(QObject):
     """Aircraft Flight Control Computer"""
 
-    def __init__(self, aircraft : AircraftVehicle) -> None:
+    def __init__(self, aircraft_id : int, aircraft : AircraftVehicle) -> None:
         super().__init__()
-        
+        self.aircraft_id = aircraft_id
         self.aircraft = aircraft
 
         self.__safezone_occupied : bool = False
@@ -26,9 +27,12 @@ class AircraftFCC(QObject):
         # self.target_speed : float = 100.0
 
         self.destinations : List[QVector3D] = [
-            QVector3D(1000, 1000, 1000),
-            QVector3D(10000, -100000, 1000),
+            QVector3D(600, 900, 1000),
+            QVector3D(-10, -10, 1000),
         ]
+        self.destinations_history : List[QVector3D] = []
+
+        self.visited : List[QVector3D] = []
         return
 
     def safezone_size(self) -> float:
@@ -40,6 +44,11 @@ class AircraftFCC(QObject):
         if occupied is not None:
             self.__safezone_occupied = occupied
         return self.__safezone_occupied
+    
+    def append_visited(self) -> None:
+        """"""
+        self.visited.append(copy(self.aircraft.position()))
+        return
 
     def update(self) -> None:
         """Updates current targetted movement angles"""
@@ -48,17 +57,32 @@ class AircraftFCC(QObject):
         destination = self.destinations[0]
         distance = dist(self.aircraft.position().toTuple(), destination.toTuple())
         if distance < self.aircraft.size():
-            self.destinations.pop(0)
+            self.destinations_history.append(self.destinations.pop(0))
             if self.destinations:
                 destination = self.destinations[0]
+                logging.info(f"Aircraft {self.aircraft.aircraft_id()} visited destination and took next one")
             else:
+                logging.info(f"Aircraft {self.aircraft.aircraft_id()} visited destination and is free now")
                 return
-        angle = degrees(atan2(
+        new_yaw_angle = degrees(atan2(
             destination.y() - self.aircraft.position().y(),
             destination.x() - self.aircraft.position().x()))
-        angle += 90
-        if angle > 180:
-            angle = angle - 180
-            angle *= -1
-        self.target_yaw_angle = angle
+        new_yaw_angle += 90
+        if new_yaw_angle > 180:
+            new_yaw_angle = new_yaw_angle - 180
+            new_yaw_angle *= -1
+        self.target_yaw_angle = new_yaw_angle
+
+        current_yaw_angle = self.aircraft.yaw_angle()
+        if abs(current_yaw_angle - new_yaw_angle) < 0.5:
+            self.target_roll_angle = 0.0
+        else:
+            current_yaw_angle = (current_yaw_angle + 180) % 360 - 180
+            target_yaw_angle = (self.target_yaw_angle + 180) % 360 - 180
+            yaw_difference = target_yaw_angle - current_yaw_angle
+            if yaw_difference > 0:
+                self.target_roll_angle = 30.0
+            else:
+                self.target_roll_angle = -30.0
+
         return
