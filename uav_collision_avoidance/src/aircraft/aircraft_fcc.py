d6f80676508888da00af84504bc74b249328d1b9
mldxo <miloszmaculewicz@gmail.com>
2024-03-14 14:26:10 +0100

    Fix target yaw angle calculation, add comments, change defaults

--- uav_collision_avoidance/src/aircraft/aircraft_fcc.py
+++ uav_collision_avoidance/src/aircraft/aircraft_fcc.py
@@ -3,7 +3,7 @@
 import logging
 from copy import copy
 from typing import List
-from math import dist, atan2, degrees
+from math import dist, atan2, degrees, radians, pi
 
 from PySide6.QtCore import QObject
 from PySide6.QtGui import QVector3D
@@ -27,8 +27,8 @@ class AircraftFCC(QObject):
         # self.target_speed : float = 100.0
 
         self.destinations : List[QVector3D] = [
-            QVector3D(600, 900, 1000),
-            QVector3D(-10, -10, 1000),
+            QVector3D(1100, 850, 1000),
+            # QVector3D(100, 100, 1000),
         ]
         self.destinations_history : List[QVector3D] = []
 
@@ -36,17 +36,17 @@ class AircraftFCC(QObject):
         return
 
     def safezone_size(self) -> float:
-        """"""
+        """Returns safezone size"""
         return self.__safezone_size
     
     def safezone_occupied(self, occupied=None) -> bool:
-        """"""
+        """Gets and/or sets safezone state"""
         if occupied is not None:
             self.__safezone_occupied = occupied
         return self.__safezone_occupied
     
     def append_visited(self) -> None:
-        """"""
+        """Appends current location to visited list"""
         self.visited.append(copy(self.aircraft.position()))
         return
 
@@ -61,8 +61,10 @@ class AircraftFCC(QObject):
             if self.destinations:
                 destination = self.destinations[0]
                 logging.info(f"Aircraft {self.aircraft.aircraft_id()} visited destination and took next one")
+                print(f"Aircraft {self.aircraft.aircraft_id()} visited destination and took next one")
             else:
                 logging.info(f"Aircraft {self.aircraft.aircraft_id()} visited destination and is free now")
+                print(f"Aircraft {self.aircraft.aircraft_id()} visited destination and is free now")
                 return
         new_yaw_angle = degrees(atan2(
             destination.y() - self.aircraft.position().y(),
@@ -73,16 +75,14 @@ class AircraftFCC(QObject):
             new_yaw_angle *= -1
         self.target_yaw_angle = new_yaw_angle
 
-        current_yaw_angle = self.aircraft.yaw_angle()
-        if abs(current_yaw_angle - new_yaw_angle) < 0.5:
+        target_yaw_rad : float = radians(new_yaw_angle)
+        current_yaw_rad : float = radians(self.aircraft.yaw_angle())
+        yaw_difference_rad = target_yaw_rad - current_yaw_rad
+        yaw_difference_rad = (yaw_difference_rad + pi) % (2 * pi) - pi
+        if yaw_difference_rad >= 0.0:
+            self.target_roll_angle = 30.0
+        elif yaw_difference_rad < 0.0:
+            self.target_roll_angle = -30.0
+        if abs(degrees(yaw_difference_rad)) < 5.0:
             self.target_roll_angle = 0.0
-        else:
-            current_yaw_angle = (current_yaw_angle + 180) % 360 - 180
-            target_yaw_angle = (self.target_yaw_angle + 180) % 360 - 180
-            yaw_difference = target_yaw_angle - current_yaw_angle
-            if yaw_difference > 0:
-                self.target_roll_angle = 30.0
-            else:
-                self.target_roll_angle = -30.0
-
         return
