1fc197cefef78ca554e2bb70b5da028d3c7662bc
mldxo <miloszmaculewicz@gmail.com>
2024-03-17 15:14:17 +0100

    Add docstring, split to long line, fix import order, update version setting method

--- uav_collision_avoidance/main.py
+++ uav_collision_avoidance/main.py
@@ -1,25 +1,25 @@
-# main.py
-
-with open("../version", "r") as file:
-    version = file.read()
-
-from pathlib import Path
-Path("logs").mkdir(parents=True, exist_ok=True)
-
-import logging
-logging.basicConfig(filename="logs/simulation.log", level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(filename)s - %(message)s")
+"""Main module for UAV Collision Avoidance application"""
 
 import sys
+import logging
 import platform
-if platform.system() == "Windows":
-    import ctypes
-    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(f"io.github.mldxo.uav-collision-avoidance.{version}")
-logging.info(f"Detected platform: {platform.system()}")
+from pathlib import Path
 
 from PySide6.QtWidgets import QApplication
+from version import __version__ as version
+from src.simulation.simulation import Simulation, SimulationSettings
 
-from src.simulation.simulation_settings import SimulationSettings
-from src.simulation.simulation import Simulation
+Path("logs").mkdir(parents=True, exist_ok=True)
+logging.basicConfig(
+    filename="logs/simulation.log",
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(filename)s - %(message)s")
+
+if platform.system() == "Windows":
+    import ctypes
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(
+        f"io.github.mldxo.uav-collision-avoidance.{version}")
+logging.info("Detected platform: %s", platform.system())
 
 def main(args):
     """Executes main function"""
@@ -27,7 +27,7 @@ def main(args):
     app.setApplicationName("UAV Collsion Avoidance")
     app.setApplicationVersion(version)
     SimulationSettings.screen_resolution = app.primaryScreen().size()
-    logging.info("Creating simulation")
+    logging.info("%s %s", app.applicationName(), app.applicationVersion())
     sim = Simulation()
     sim.run_realtime()
     sys.exit(app.exec())
