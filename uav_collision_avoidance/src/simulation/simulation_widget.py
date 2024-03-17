9cae19ed65c23c6ac1fecc7c4d262af729225b38
mldxo <miloszmaculewicz@gmail.com>
2024-03-17 15:22:43 +0100

    Split to long lines, update imports, add destination setting and drawing

--- uav_collision_avoidance/src/simulation/simulation_widget.py
+++ uav_collision_avoidance/src/simulation/simulation_widget.py
@@ -1,23 +1,33 @@
-# simulation_widget.py
+""""""
 
 from math import cos, radians
 from typing import List
 
 from PySide6.QtCore import Qt, QPointF, Signal
-from PySide6.QtGui import QPaintEvent, QPainter, QKeyEvent, QMouseEvent, QIcon, QPixmap, QCloseEvent
+from PySide6.QtGui import QPaintEvent, QPainter, QKeyEvent, \
+    QMouseEvent, QIcon, QPixmap, QCloseEvent, QVector3D
 from PySide6.QtWidgets import QWidget, QApplication
 
+from src.aircraft.aircraft import Aircraft
+from src.aircraft.aircraft_fcc import AircraftFCC
+from src.aircraft.aircraft_vehicle import AircraftVehicle
 from src.aircraft.aircraft_render import AircraftRender
 from src.simulation.simulation_fps import SimulationFPS
 from src.simulation.simulation_settings import SimulationSettings
 
 class SimulationWidget(QWidget):
     """Main widget representing the simulation"""
-    
     stop_signal = Signal(str)
-
-    def __init__(self, aircrafts : List[AircraftRender], simulation_fps : SimulationFPS, simulation_state : SimulationSettings):
+    def __init__(self, aircrafts : List[Aircraft],
+                 simulation_fps : SimulationFPS, simulation_state : SimulationSettings):
         super().__init__()
+        self.aircrafts = aircrafts
+        self.aircraft_vehicles : List[AircraftVehicle] = [
+            aircraft.vehicle() for aircraft in self.aircrafts]
+        self.aircraft_fccs : List[AircraftFCC] = [
+            aircraft.fcc() for aircraft in self.aircrafts]
+        self.aircraft_renders : List[AircraftRender] = [
+            aircraft.render() for aircraft in self.aircrafts]
         self.simulation_fps = simulation_fps
         self.simulation_state = simulation_state
 
@@ -30,13 +40,17 @@ class SimulationWidget(QWidget):
             window_width,
             window_height)
         self.setStyleSheet("background-color: white;")
-        self.setWindowTitle("UAV Collision Avoidance")
+        self.setWindowTitle(QApplication.applicationName() + " " + QApplication.applicationVersion())
 
         self.icon = QIcon()
         self.icon.addPixmap(self.simulation_state.aircraft_pixmap, QIcon.Mode.Normal, QIcon.State.Off)
         self.setWindowIcon(self.icon)
-
-        self.aircrafts = aircrafts
+        return
+    
+    def update_aircrafts(self) -> None:
+        """Updates aircrafts position"""
+        for aircraft in self.aircraft_renders:
+            aircraft.update()
         return
 
     def paintEvent(self, event: QPaintEvent) -> None:
@@ -44,7 +58,7 @@ class SimulationWidget(QWidget):
         if self.simulation_state.aircraft_pixmap.isNull():
             return super().paintEvent(event)
         self.simulation_fps.count_frame()
-        for aircraft in self.aircrafts:
+        for aircraft in self.aircraft_renders:
             pixmap : QPixmap = self.simulation_state.aircraft_pixmap.scaled(
                 aircraft.size * abs(cos(radians(aircraft.roll_angle))),
                 aircraft.size * abs(cos(radians(aircraft.pitch_angle)))
@@ -64,19 +78,39 @@ class SimulationWidget(QWidget):
                 aircraft.size * abs(cos(radians(aircraft.roll_angle))),
                 aircraft.size * abs(cos(radians(aircraft.pitch_angle))))
             painter.end()
+            painter = QPainter(self)
+            painter.setBrush(Qt.BrushStyle.SolidPattern)
+            for destination in aircraft.fcc.destinations:
+                painter.drawEllipse(
+                    destination.x() * self.simulation_state.gui_scale - 5,
+                    destination.y() * self.simulation_state.gui_scale - 5,
+                    10, 10)
+            painter.end()
         return super().paintEvent(event)
 
     def mousePressEvent(self, event: QMouseEvent) -> None:
         """Qt method controlling single click mouse input"""
+        print("click x: " + str(event.pos().x() / self.simulation_state.gui_scale) + "; y: " + str(event.pos().y() / self.simulation_state.gui_scale))
         if event.button() == Qt.MouseButton.LeftButton:
-            print("click x: " + str(event.pos().x()) + "; y: " + str(event.pos().y()))
-        elif event.button() == Qt.MouseButton.LeftButton:
-            QApplication.beep() # does not work in Linux
+            self.aircraft_fccs[0].push_destination_top(QVector3D(
+                float(event.pos().x() / self.simulation_state.gui_scale),
+                float(event.pos().y() / self.simulation_state.gui_scale),
+                1000.0))
+        elif event.button() == Qt.MouseButton.RightButton:
+            self.aircraft_fccs[0].append_destination(QVector3D(
+                float(event.pos().x() / self.simulation_state.gui_scale),
+                float(event.pos().y() / self.simulation_state.gui_scale),
+                1000.0))
+        elif event.button() == Qt.MouseButton.MiddleButton:
+            self.aircraft_vehicles[0].teleport(
+                float(event.pos().x() / self.simulation_state.gui_scale),
+                float(event.pos().y() / self.simulation_state.gui_scale),
+                1000.0)
         return super().mousePressEvent(event)
     
     def mouseDoubleClickEvent(self, event: QMouseEvent) -> None:
         """Qt method controlling double click mouse input"""
-        self.close()
+        QApplication.beep()
         return super().mouseDoubleClickEvent(event)
 
     def keyPressEvent(self, event: QKeyEvent) -> None:
@@ -87,26 +121,24 @@ class SimulationWidget(QWidget):
             self.simulation_state.toggle_pause()
         elif event.key() == Qt.Key.Key_R:
             self.simulation_state.reset()
-        step = 5
-        for aircraft in self.aircrafts:
-            if aircraft.aircraft_id == "0":
-                if event.key() == Qt.Key.Key_A:
-                    aircraft.move(-step, 0)
-                elif event.key() == Qt.Key.Key_D:
-                    aircraft.move(step, 0)
-                elif event.key() == Qt.Key.Key_W:
-                    aircraft.move(0, -step)
-                elif event.key() == Qt.Key.Key_S:
-                    aircraft.move(0, step)
-            elif aircraft.aircraft_id == "1":
-                if event.key() == Qt.Key.Key_Left:
-                    aircraft.move(-step, 0)
-                elif event.key() == Qt.Key.Key_Right:
-                    aircraft.move(step, 0)
-                elif event.key() == Qt.Key.Key_Up:
-                    aircraft.move(0, -step)
-                elif event.key() == Qt.Key.Key_Down:
-                    aircraft.move(0, step)
+        if self.aircrafts[0]:
+            if event.key() == Qt.Key.Key_A:
+                self.aircraft_fccs[0].target_yaw_angle = -90.0
+            elif event.key() == Qt.Key.Key_D:
+                self.aircraft_fccs[0].target_yaw_angle = 90.0
+            elif event.key() == Qt.Key.Key_W:
+                self.aircraft_fccs[0].target_yaw_angle = 0.0
+            elif event.key() == Qt.Key.Key_S:
+                self.aircraft_fccs[0].target_yaw_angle = 180.0
+        if self.aircrafts[1]:
+            if event.key() == Qt.Key.Key_Left:
+                self.aircraft_fccs[1].target_yaw_angle = -90.0
+            elif event.key() == Qt.Key.Key_Right:
+                self.aircraft_fccs[1].target_yaw_angle = 90.0
+            elif event.key() == Qt.Key.Key_Up:
+                self.aircraft_fccs[1].target_yaw_angle = 0.0
+            elif event.key() == Qt.Key.Key_Down:
+                self.aircraft_fccs[1].target_yaw_angle = 180.0
         return super().keyPressEvent(event)
 
     def closeEvent(self, event: QCloseEvent) -> None:
@@ -114,9 +146,3 @@ class SimulationWidget(QWidget):
         self.stop_signal.emit("stop")
         event.accept()
         return super().closeEvent(event)
-
-    def update_aircrafts(self) -> None:
-        """Updates aircrafts position"""
-        for aircraft in self.aircrafts:
-            aircraft.update()
-        return
