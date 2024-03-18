a3e8a7cb4b3100a164bff94f08490f0f93704789
mldxo <miloszmaculewicz@gmail.com>
2024-03-18 16:52:38 +0100

    Prevent unsufficient write rights throwing exceptions

--- uav_collision_avoidance/main.py
+++ uav_collision_avoidance/main.py
@@ -10,12 +10,18 @@ from PySide6.QtWidgets import QApplication
 from version import __version__ as version
 from src.simulation.simulation import Simulation, SimulationSettings
 
-start_time = datetime.datetime.now().strftime("%Y-%m-%d")
-Path("logs").mkdir(parents=True, exist_ok=True)
-logging.basicConfig(
-    filename=f"logs/simulation-{start_time}.log",
-    level=logging.DEBUG,
-    format="%(asctime)s - %(levelname)s - %(filename)s - %(message)s")
+try:
+    start_time = datetime.datetime.now().strftime("%Y-%m-%d")
+    Path("logs").mkdir(parents=True, exist_ok=True)
+    logging.basicConfig(
+        filename=f"logs/simulation-{start_time}.log",
+        level=logging.DEBUG,
+        format="%(asctime)s - %(levelname)s - %(filename)s - %(message)s")
+except:
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s - %(levelname)s - %(filename)s - %(message)s",
+        handlers=[logging.StreamHandler()])
 
 if platform.system() == "Windows":
     import ctypes
