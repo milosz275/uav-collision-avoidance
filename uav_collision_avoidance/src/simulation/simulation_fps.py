8d88c6c820f6370461e8c4bdf57f8e02ea905b1c
mldxo <miloszmaculewicz@gmail.com>
2024-03-19 11:04:40 +0100

    Add mutex and mutex locker for fps count, fill docstring

--- uav_collision_avoidance/src/simulation/simulation_fps.py
+++ uav_collision_avoidance/src/simulation/simulation_fps.py
@@ -1,6 +1,6 @@
-""""""
+"""Simulation frames per second counter thread module"""
 
-from PySide6.QtCore import QThread, QTime
+from PySide6.QtCore import QThread, QTime, QMutex, QMutexLocker
 from PySide6.QtGui import QCloseEvent
 
 from src.simulation.simulation_state import SimulationState
@@ -10,8 +10,10 @@ class SimulationFPS(QThread):
 
     def __init__(self, parent, simulation_state : SimulationState) -> None:
         super(SimulationFPS, self).__init__(parent)
+        self.__mutex : QMutex = QMutex()
+        self.__counted_frames : int = 0
+
         self.simulation_state = simulation_state
-        self.counted_frames : int = 0
         self.previous_timestamp = QTime.currentTime()
         return
         
@@ -20,9 +22,9 @@ class SimulationFPS(QThread):
         while not self.isInterruptionRequested():
             start_timestamp = QTime.currentTime()
             elapsed_time : float = self.previous_timestamp.msecsTo(start_timestamp)
-            if self.counted_frames > 0:
-                self.simulation_state.fps = self.counted_frames / elapsed_time * 1000
-                self.counted_frames = 0
+            if self.counted_frames() > 0:
+                self.simulation_state.fps = self.counted_frames() / elapsed_time * 1000
+                self.reset_frames()
             else:
                 self.simulation_state.fps = 0.0
             self.previous_timestamp = QTime.currentTime()
@@ -31,7 +33,18 @@ class SimulationFPS(QThread):
 
     def count_frame(self) -> None:
         """Increments fps count"""
-        self.counted_frames += 1
+        QMutexLocker(self.__mutex)
+        self.__counted_frames += 1
+
+    def reset_frames(self) -> None:
+        """Resets fps count"""
+        QMutexLocker(self.__mutex)
+        self.__counted_frames = 0
+
+    def counted_frames(self) -> int:
+        """Returns counted frames"""
+        QMutexLocker(self.__mutex)
+        return self.__counted_frames
 
     def closeEvent(self, event: QCloseEvent) -> None:
         """Qt method performed on the main window close event"""
