717ae7a1388ed6d969aab597187806794f249d96
mldxo <miloszmaculewicz@gmail.com>
2024-03-25 22:02:37 +0100

    Add collision detection

--- uav_collision_avoidance/src/simulation/simulation_adsb.py
+++ uav_collision_avoidance/src/simulation/simulation_adsb.py
@@ -3,6 +3,7 @@
 from typing import List
 
 from PySide6.QtCore import QThread, QTime
+from PySide6.QtGui import QVector3D
 from PySide6.QtWidgets import QMainWindow
 
 from src.aircraft.aircraft import Aircraft
@@ -23,27 +24,57 @@ class SimulationADSB(QThread):
         
     def run(self) -> None:
         """Runs ADS-B simulation thread with precise timeout"""
+        aircraft1 : AircraftVehicle = self.aircraft_vehicles[0]
+        aircraft2 : AircraftVehicle = self.aircraft_vehicles[1]
+
         while not self.isInterruptionRequested():
             start_timestamp = QTime.currentTime()
-            if not self.simulation_state.is_paused and self.simulation_state.adsb_report:
+            if not self.simulation_state.is_paused:
                 self.adsb_cycles += 1
                 self.simulation_state.update_adsb_settings()
+
+                relative_position = aircraft1.position - aircraft2.position
+                speed_difference = aircraft1.speed - aircraft2.speed
+                time_to_closest_approach = -(QVector3D.dotProduct(relative_position, speed_difference) / QVector3D.dotProduct(speed_difference, speed_difference))
+                print("Time to closest approach: " + "{:.2f}".format(time_to_closest_approach) + "s")
+
+                if time_to_closest_approach > 0:
+                    # miss distance at closest approach
+                    speed_difference_unit = speed_difference.normalized()
+                    miss_distance_vector : QVector3D = QVector3D.crossProduct(
+                        speed_difference_unit,
+                        QVector3D.crossProduct(relative_position, speed_difference_unit))
+                    print("Miss distance at closest approach: " + "{:.2f}".format(miss_distance_vector.length()) + "m")
+
+                    # resolve confict condition
+                    minimum_separation = aircraft1.size / 2 + aircraft2.size / 2
+                    unresolved_region = minimum_separation - miss_distance_vector.length()
+                    if unresolved_region > 0:
+                        print("Conflict condition found")
+
                 for aircraft in self.aircraft_vehicles:
+                    # path
                     self.aircraft_fccs[aircraft.aircraft_id].append_visited()
-                    if aircraft.aircraft_id == 0:
-                        print("Aircraft id: " + str(aircraft.aircraft_id) +
-                            "; speed: " + "{:.2f}".format(aircraft.absolute_speed) +
-                            "; target speed: " + "{:.2f}".format(self.aircraft_fccs[aircraft.aircraft_id].target_speed) +
-                            "; x: " + "{:.2f}".format(aircraft.position.x()) +
-                            "; y: " + "{:.2f}".format(aircraft.position.y()) +
-                            "; yaw angle: " + "{:.2f}".format(aircraft.yaw_angle) +
-                            "; target yaw angle: " + "{:.2f}".format(self.aircraft_fccs[aircraft.aircraft_id].target_yaw_angle) +
-                            "; pitch angle: " + "{:.2f}".format(aircraft.pitch_angle) +
-                            "; roll angle: " + "{:.2f}".format(aircraft.roll_angle) +
-                            "; target roll angle: " + "{:.2f}".format(self.aircraft_fccs[aircraft.aircraft_id].target_roll_angle) +
-                            "; distance covered: " + "{:.2f}".format(aircraft.distance_covered) +
-                            "; fps: " + "{:.2f}".format(self.simulation_state.fps) +
-                            "; t: " + str(self.adsb_cycles) +
-                            "; phys: " + str(self.simulation_state.physics_cycles))
+
+                    # console output
+                    if self.simulation_state.adsb_report and aircraft.aircraft_id == 0:
+                        self.print_adsb_report(aircraft)
             self.msleep(max(0, self.simulation_state.adsb_threshold - start_timestamp.msecsTo(QTime.currentTime())))
         return super().run()
+
+    def print_adsb_report(self, aircraft : AircraftVehicle) -> None:
+        """Prints ADS-B report for the aircraft to the console"""
+        print("Aircraft id: " + str(aircraft.aircraft_id) +
+            "; speed: " + "{:.2f}".format(aircraft.absolute_speed) +
+            "; target speed: " + "{:.2f}".format(self.aircraft_fccs[aircraft.aircraft_id].target_speed) +
+            "; x: " + "{:.2f}".format(aircraft.position.x()) +
+            "; y: " + "{:.2f}".format(aircraft.position.y()) +
+            "; yaw angle: " + "{:.2f}".format(aircraft.yaw_angle) +
+            "; target yaw angle: " + "{:.2f}".format(self.aircraft_fccs[aircraft.aircraft_id].target_yaw_angle) +
+            "; pitch angle: " + "{:.2f}".format(aircraft.pitch_angle) +
+            "; roll angle: " + "{:.2f}".format(aircraft.roll_angle) +
+            "; target roll angle: " + "{:.2f}".format(self.aircraft_fccs[aircraft.aircraft_id].target_roll_angle) +
+            "; distance covered: " + "{:.2f}".format(aircraft.distance_covered) +
+            "; fps: " + "{:.2f}".format(self.simulation_state.fps) +
+            "; t: " + str(self.adsb_cycles) +
+            "; phys: " + str(self.simulation_state.physics_cycles))
