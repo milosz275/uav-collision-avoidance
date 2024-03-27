9ed3b263eb438a213059aa4e17effe8e6bcef1f1
mldxo <miloszmaculewicz@gmail.com>
2024-03-27 23:01:40 +0100

    Add separate cycle method, add head-on collision check,

--- uav_collision_avoidance/src/simulation/simulation_adsb.py
+++ uav_collision_avoidance/src/simulation/simulation_adsb.py
@@ -1,5 +1,6 @@
 """Simulation ADS-B system simulation thread module"""
 
+import logging
 from typing import List
 
 from PySide6.QtCore import QThread, QTime
@@ -24,49 +25,58 @@ class SimulationADSB(QThread):
         
     def run(self) -> None:
         """Runs ADS-B simulation thread with precise timeout"""
-        aircraft1 : AircraftVehicle = self.aircraft_vehicles[0]
-        aircraft2 : AircraftVehicle = self.aircraft_vehicles[1]
-
         while not self.isInterruptionRequested():
             start_timestamp = QTime.currentTime()
-            if not self.simulation_state.is_paused:
-                self.adsb_cycles += 1
-                self.simulation_state.update_adsb_settings()
-
-                relative_position = aircraft1.position - aircraft2.position
-                speed_difference = aircraft1.speed - aircraft2.speed
-                time_to_closest_approach = -(QVector3D.dotProduct(relative_position, speed_difference) / QVector3D.dotProduct(speed_difference, speed_difference))
-                print("Time to closest approach: " + "{:.2f}".format(time_to_closest_approach) + "s")
-
-                if time_to_closest_approach > 0:
-                    # miss distance at closest approach
-                    speed_difference_unit = speed_difference.normalized()
-                    miss_distance_vector : QVector3D = QVector3D.crossProduct(
-                        speed_difference_unit,
-                        QVector3D.crossProduct(relative_position, speed_difference_unit))
-                    print("Miss distance at closest approach: " + "{:.2f}".format(miss_distance_vector.length()) + "m")
-
-                    # resolve confict condition
-                    minimum_separation = 200
-                    unresolved_region = minimum_separation - miss_distance_vector.length()
-                    if unresolved_region > 0:
-                        print("Conflict condition detected")
-                    
-                    # probable collision
-                    collision_distance = aircraft1.size / 2 + aircraft2.size / 2
-                    collision_region = collision_distance - miss_distance_vector.length()
-                    if collision_region > 0:
-                        print("Probable collision detected")
-
-                for aircraft in self.aircraft_vehicles:
-                    # path
-                    self.aircraft_fccs[aircraft.aircraft_id].append_visited()
-
-                    # console output
-                    if self.simulation_state.adsb_report and aircraft.aircraft_id == 0:
-                        self.print_adsb_report(aircraft)
+            self.cycle()
             self.msleep(max(0, self.simulation_state.adsb_threshold - start_timestamp.msecsTo(QTime.currentTime())))
         return super().run()
+    
+    def cycle(self) -> None:
+        """Executes ADS-B simulation cycle"""
+        aircraft_vehicle_1 : AircraftVehicle = self.aircraft_vehicles[0]
+        aircraft_vehicle_2 : AircraftVehicle = self.aircraft_vehicles[1]
+
+        if not self.simulation_state.is_paused:
+            self.adsb_cycles += 1
+            self.simulation_state.update_adsb_settings()
+
+            relative_position = aircraft_vehicle_1.position - aircraft_vehicle_2.position
+            speed_difference = aircraft_vehicle_1.speed - aircraft_vehicle_2.speed
+            time_to_closest_approach = -(QVector3D.dotProduct(relative_position, speed_difference) / QVector3D.dotProduct(speed_difference, speed_difference))
+            print("Time to closest approach: " + "{:.2f}".format(time_to_closest_approach) + "s")
+
+            if time_to_closest_approach > 0:
+                # miss distance at closest approach
+                speed_difference_unit = speed_difference.normalized()
+                miss_distance_vector : QVector3D = QVector3D.crossProduct(
+                    speed_difference_unit,
+                    QVector3D.crossProduct(relative_position, speed_difference_unit))
+                print("Miss distance at closest approach: " + "{:.2f}".format(miss_distance_vector.length()) + "m")
+
+                if miss_distance_vector.length() == 0:
+                    print("Head-on collision detected")
+
+                # resolve confict condition
+                minimum_separation = 100
+                unresolved_region = minimum_separation - miss_distance_vector.length()
+                if unresolved_region > 0:
+                    print("Conflict condition detected")
+                    if not self.aircraft_fccs[0].evade_maneuver:
+                        self.aircraft_fccs[0].apply_evade_maneuver()
+                
+                # probable collision
+                collision_distance = aircraft_vehicle_1.size / 2 + aircraft_vehicle_2.size / 2
+                collision_region = collision_distance - miss_distance_vector.length()
+                if collision_region > 0:
+                    print("Probable collision detected")
+
+            for aircraft in self.aircraft_vehicles:
+                # path
+                self.aircraft_fccs[aircraft.aircraft_id].append_visited()
+
+                # console output
+                if self.simulation_state.adsb_report and aircraft.aircraft_id == 0:
+                    self.print_adsb_report(aircraft)
 
     def print_adsb_report(self, aircraft : AircraftVehicle) -> None:
         """Prints ADS-B report for the aircraft to the console"""
