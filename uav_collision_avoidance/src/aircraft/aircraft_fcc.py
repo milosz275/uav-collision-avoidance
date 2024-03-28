8f9e3e077b2049b2d6538020d2c3486d29e8dee8
mldxo <miloszmaculewicz@gmail.com>
2024-03-28 21:33:40 +0100

    Fix calculation of vector sharing resolution

--- uav_collision_avoidance/src/aircraft/aircraft_fcc.py
+++ uav_collision_avoidance/src/aircraft/aircraft_fcc.py
@@ -28,6 +28,7 @@ class AircraftFCC(QObject):
         self.__target_speed : float = self.aircraft.absolute_speed
 
         self.__evade_maneuver : bool = False
+        self.vector_sharing_resolution : QVector3D | None = None
 
         self.destinations : deque[QVector3D] = deque()
         self.destinations_history : List[QVector3D] = []
@@ -78,12 +79,12 @@ class AircraftFCC(QObject):
         else:
             logging.info("Aircraft %s applying evade maneuver", self.aircraft.aircraft_id)
             self.__evade_maneuver = True
-            vector_sharing_resolution : QVector3D
             if self.aircraft_id == 0:
-                vector_sharing_resolution = (opponent_speed.length() * unresolved_region * -(miss_distance_vector)) / (self.aircraft.speed.length() + opponent_speed.length() * miss_distance_vector.length())
+                self.vector_sharing_resolution = (opponent_speed.length() * unresolved_region * -(miss_distance_vector)) / ((self.aircraft.speed.length() + opponent_speed.length()) * miss_distance_vector.length())
             else:
-                vector_sharing_resolution = (opponent_speed.length() * unresolved_region * miss_distance_vector) / (opponent_speed.length() + self.aircraft.speed.length() * miss_distance_vector.length())
-            #print("Vector sharing resolution: ", vector_sharing_resolution)
+                self.vector_sharing_resolution = (opponent_speed.length() * unresolved_region * miss_distance_vector) / ((opponent_speed.length() + self.aircraft.speed.length()) * miss_distance_vector.length())
+            print("Vector sharing resolution: ", self.vector_sharing_resolution)
+            
             # todo: calculate turn radius, apply roll angle,
             # add corner enter position,
             # add maneuver end position, 
