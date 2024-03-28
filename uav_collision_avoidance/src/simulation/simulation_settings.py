8faab5c56dd26cc20fe5a1905f5265bc5206e7c9
mldxo <miloszmaculewicz@gmail.com>
2024-03-28 17:45:34 +0100

    Remove unused methods

--- uav_collision_avoidance/src/simulation/simulation_settings.py
+++ uav_collision_avoidance/src/simulation/simulation_settings.py
@@ -18,20 +18,3 @@ class SimulationSettings:
     def __init__(cls) -> None:
         """Initialises Settings using screen resolution"""
         cls.resolution = (int(cls.screen_resolution.width() * 0.6), int(cls.screen_resolution.height() * 0.75))
-    
-    @classmethod
-    def set_resolution(cls, width, height) -> None:
-        """Sets the resolution"""
-        cls.resolution = (width - 10, height - 10)
-        return
-    
-    @classmethod
-    def get_resolution(cls) -> tuple[float, float]:
-        """Gets the resolution"""
-        return (cls.resolution[0], cls.resolution[1])
-
-    @classmethod
-    def set_simulation_frequency(cls, simulation_frequency) -> None:
-        """Sets the simulations per second"""
-        cls.simulation_frequency = simulation_frequency
-        return
