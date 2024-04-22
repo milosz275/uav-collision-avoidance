c944edad8d04a8aa270f1e333b95c99b84c83aa7
mldxo <miloszmaculewicz@gmail.com>
2024-04-22 16:12:31 +0200

    Fix version check on built executable

--- uav_collision_avoidance/version.py
+++ uav_collision_avoidance/version.py
@@ -3,7 +3,10 @@
 import toml
 
 def get_version():
-    pyproject = toml.load("pyproject.toml")
-    return pyproject['project']['version']
+    try:
+        pyproject = toml.load("pyproject.toml")
+        return pyproject["project"]["version"]
+    except:
+        return ""
 
 __version__ = get_version()
