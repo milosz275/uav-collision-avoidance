a534c3408e3ef79a22a446cb0e404838396a5f5f
mldxo <miloszmaculewicz@gmail.com>
2024-04-29 12:09:21 +0200

    Change id name

--- uav_collision_avoidance/src/simulation/simulation_physics.py
+++ uav_collision_avoidance/src/simulation/simulation_physics.py
@@ -132,10 +132,10 @@ class SimulationPhysics(QThread):
         assert elapsed_time > 0.0
         for aircraft in self.aircraft_vehicles:
             # flight control computer
-            id : int = aircraft.aircraft_id
-            fcc : AircraftFCC = self.aircraft_fccs[id]
-            cause_collision = self.simulation_state.first_cause_collision if id == 0 else self.simulation_state.second_cause_collision
-            fcc.update() if not cause_collision else fcc.update_target(self.aircraft_vehicles[1 - id].position + self.aircraft_vehicles[1 - id].speed)
+            aircraft_id : int = aircraft.aircraft_id
+            fcc : AircraftFCC = self.aircraft_fccs[aircraft_id]
+            cause_collision = self.simulation_state.first_cause_collision if aircraft_id == 0 else self.simulation_state.second_cause_collision
+            fcc.update() if not cause_collision else fcc.update_target(self.aircraft_vehicles[1 - aircraft_id].position + self.aircraft_vehicles[1 - aircraft_id].speed)
             
             # speed
             current_speed = aircraft.absolute_speed
