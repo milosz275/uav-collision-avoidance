a41dc6a04e6a2ee97fbee07d209739fa1df66052
mldxo <miloszmaculewicz@gmail.com>
2024-04-29 16:53:44 +0200

    Add simulation physics test

--- tests/test_headless.py
+++ tests/test_headless.py
@@ -1,7 +1,21 @@
 import pytest
+import sys
+from PySide6.QtWidgets import QApplication
 from main import *
+from . import Simulation, SimulationSettings
+
 
 def test_headless():
     with pytest.raises(SystemExit) as e:
         main("headless")
+    assert e.value.code == 0
+
+def test_physics():
+    with pytest.raises(SystemExit) as e:
+        app = QApplication()
+        SimulationSettings.__init__()
+        sim = Simulation(headless = True)
+        assert sim is not None
+        QApplication.shutdown(app)
+        sys.exit(0)
     assert e.value.code == 0
