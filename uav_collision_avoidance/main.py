30c8ee9c260bc77f7e0171a94d3fbac3fc91d6e7
mldxo <miloszmaculewicz@gmail.com>
2024-05-15 19:00:35 +0200

    Update example simulation data

--- uav_collision_avoidance/main.py
+++ uav_collision_avoidance/main.py
@@ -79,7 +79,7 @@ def main(arg = None) -> None:
             QApplication.shutdown(app)
             sys.exit(0)
         elif args[0] == "load":
-            file_path : str = "data/simulation-2024-05-14-11-42-37.csv"
+            file_path : str = "data/simulation-2024-05-15-17-37-59.csv"
             if len(args) > 1:
                 file_path = args[1]
                 test_id : int = 0
