954c1134a6c7c85dd35c71a147f6b49ec4150f13
mldxo <miloszmaculewicz@gmail.com>
2024-05-28 14:26:00 +0200

    Ignore internal warnings

--- tests/test_headless.py
+++ tests/test_headless.py
@@ -12,6 +12,7 @@ def test_headless():
 test_path : str = "data/simulation-2024-05-28-12-13-58.csv"
 lowest_frequency_tested : int = 10
 highest_frequency_tested : int = 100
+pytestmark = pytest.mark.filterwarnings("error")
 
 @pytest.fixture
 def tester(request):
@@ -24,7 +25,6 @@ class Test:
        tester.test_headless_load()
        assert 1
 
-@pytest.mark.filterwarnings("ignore:PytestCollectionWarning")
 class TestHeadless:
     """Test headless simulation"""
     def __init__(self, simulation_frequency : float):
