faf8dca41ccbbbeec7ae0903bccad3e05f2ff94e
mldxo <miloszmaculewicz@gmail.com>
2024-05-28 14:19:39 +0200

    Add simulation frequency class method

--- uav_collision_avoidance/src/simulation/simulation_settings.py
+++ uav_collision_avoidance/src/simulation/simulation_settings.py
@@ -8,9 +8,9 @@ class SimulationSettings:
     screen_resolution : QSize | None = None
     resolution : tuple
     g_acceleration : float = 9.81
-    simulation_frequency : float = 100.0
+    simulation_frequency : float = 100.0 # Hz
     simulation_threshold : float = 1000.0 / simulation_frequency
-    gui_render_frequency : float = 100.0
+    gui_render_frequency : float = 100.0 # Hz, fps
     gui_render_threshold : float =  1000.0 / gui_render_frequency
     adsb_threshold : float = 1000.0
 
@@ -19,3 +19,9 @@ class SimulationSettings:
         """Initializes Settings using screen resolution"""
         if cls.screen_resolution is not None:
             cls.resolution = (int(cls.screen_resolution.width() * 0.6), int(cls.screen_resolution.height() * 0.75))
+
+    @classmethod
+    def set_simulation_frequency(cls, frequency : float) -> None:
+        """Sets the simulation frequency"""
+        cls.simulation_frequency = frequency
+        cls.simulation_threshold = 1000.0 / frequency
